19e757d21613bf85c47a4e3f62bf5bd9faae96db
K2 <K2@Black_Pearl>
2021-01-13 14:31:12 +0530

    Add Setter, Getter & Override HashCode & Equals

--- CGO_Test/src/DTO/Employee.java
+++ CGO_Test/src/DTO/Employee.java
@@ -7,8 +7,65 @@ public class Employee {
 	private Double employeeSalary;
 	
 	private String designation;
-	
-	
-	
-	
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(Double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((designation == null) ? 0 : designation.hashCode());
+		result = prime * result + ((employeeName == null) ? 0 : employeeName.hashCode());
+		result = prime * result + ((employeeSalary == null) ? 0 : employeeSalary.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (designation == null) {
+			if (other.designation != null)
+				return false;
+		} else if (!designation.equals(other.designation))
+			return false;
+		if (employeeName == null) {
+			if (other.employeeName != null)
+				return false;
+		} else if (!employeeName.equals(other.employeeName))
+			return false;
+		if (employeeSalary == null) {
+			if (other.employeeSalary != null)
+				return false;
+		} else if (!employeeSalary.equals(other.employeeSalary))
+			return false;
+		return true;
+	}
 }
